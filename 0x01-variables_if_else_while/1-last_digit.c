bf896545c45ebc4f378248a61440de3a4831b85f
Lydia Taban <tabanlydia@gmail.com>
2022-09-13 20:24:25 +0000

    Add program that determines if value is less than 5, is zero, or is less than 6 and not 0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,8 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, a);
 	}
-	else {
+	else
+	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
 	}
 	return (0);
