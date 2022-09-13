078cabb05e2d6f443434472ba43655226318077b
Lydia Taban <tabanlydia@gmail.com>
2022-09-13 20:46:54 +0000

    Add mixed lower and uppercase alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints mixed case alphabet
+ *
+ * Return: Always (0) for success
+ */
+int main(void){
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int n;
+
+	for (n = 0; n < 52; n++)
+	{
+		putchar(alp[n]);
+	}
+	putchar('\n');
+	return (0);
+}
