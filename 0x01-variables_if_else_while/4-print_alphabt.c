0f3f77e96b82e66cb55dc52a3dde16a19d371859
Lydia Taban <tabanlydia@gmail.com>
2022-09-13 21:37:49 +0000

    Add C program that prints alphabet with the exception of q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -13,8 +13,9 @@ int main(void)
 {
         char * alp = "abcdefghijklmnopqrstuvwxyz";
         int n = 0;
+	int alp_length = 26;
 
-	for(n = 0; n < strlen(alp); n++)
+	for(n = 0; n < alp_length; n++)
 	{
 		if(n != 'e' && n != 'q')
 		{
