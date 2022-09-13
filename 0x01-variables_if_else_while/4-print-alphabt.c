e0f0de7161d8d91d24b77244454fd42686193af2
Lydia Taban <tabanlydia@gmail.com>
2022-09-13 21:08:36 +0000

    Add C program that prints alphabet with the exception of q and e

---
+++ 0x01-variables_if_else_while/4-print-alphabt.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print the alphabet with an exception of q and e
+ *
+ */
+int main(void)
+{
+        char alp[52] = "abcdefghijklmnopqrstuvwxyz";
+        int arrLen = sizeof alp[52] / sizeof alp[0];
+
+
+        int n = 0;
+        while (n < arrLen && alp[n] != 'e' && alp[n] != 'q')
+        {
+                printf("%d\n", alp[n]);
+                n++;
+        }
+        return (0);
+}
