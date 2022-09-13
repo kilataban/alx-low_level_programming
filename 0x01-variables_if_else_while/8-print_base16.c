046bbbf86c9075aa3f5e80f67a6f5d4bda2fd6b9
Lydia Taban <tabanlydia@gmail.com>
2022-09-13 23:04:54 +0000

    Add C program that prints out all base 16 digits in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers of base 16 in lowercase then \n
+ *
+ * Return: Always (0) for success
+ */
+
+int main(void)
+{
+	int a;
+
+	for(a = 48; a < 58; a++)
+	{
+		putchar(a);
+	}
+	for(a = 97; a < 103; a++)
+	{
+		putchar(a);
+	}
+
+	putchar('\n');
+	return (0);
+}
