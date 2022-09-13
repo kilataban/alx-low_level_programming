b79ddb995a659ebfa84d439c137d56c6a9eadc47
Lydia Taban <tabanlydia@gmail.com>
2022-09-13 22:05:11 +0000

    Add C program that prints all single digit base ten numbers from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit base 10 numbers from 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	putchar('\n');
+	return (0);
+}
