57b8ae3e3afadab5f3e2fac584c29568a7304b00
Lydia Taban <tabanlydia@gmail.com>
2022-09-13 22:50:49 +0000

    Add C program that reverses the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * main - print the lowercase alphabet in reverse followed by a new line
+ *
+ * Return: Always (0) for Success
+ *
+ */
+
+int main(void)
+{
+	int m;
+
+	for(m = 122; m > 96; m--){
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
