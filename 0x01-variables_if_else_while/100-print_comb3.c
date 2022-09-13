199a8672454625688a8fe5f3ccf2509486f80a5d
Lydia Taban <tabanlydia@gmail.com>
2022-09-13 23:20:05 +0000

    Add C program that creates every possible two digit combination seperated by commas

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - prints all possible combinations of 2 digits.
+ *
+ * Return: Always (0) means Success 
+ */
+int main(void)
+{
+	int a, b;
+
+	a = 48;
+	b = 48;
+
+	while (b < 58)
+	{
+		a = 48;
+		while (a < 58)
+		{
+			if (b != a && b < a)
+			{
+				putchar(b);
+				putchar(a);
+				if (a == 57 && b == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			a++;
+		}
+		b++;
+	}
+	putchar('\n');
+	return (0);
+}
