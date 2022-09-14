10747e1ba3476b481f7c4f13dfe39dbfb68425b0
Lydia Taban <tabanlydia@gmail.com>
2022-09-14 17:29:21 +0000

    Add function that prints 10 times the alphabet in lowercase, followed by \n

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_10x_alphabet - print the alphabet 10 times
+ *
+ */
+
+void print_10x_alphabet(void)
+{
+	int alph;
+	int alph2;
+
+	for (alph = 0; alph <= 9; alph++)
+	{
+		for (alph2 = 'a'; alph2 <= 'z'; alph2++)
+		{
+			_putchar(alph2);
+		}
+		_putchar('\n');
+	}
+}
