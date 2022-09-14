1f13bcb85cb98960a152b1fb70c75a9ba598eceb
Lydia Taban <tabanlydia@gmail.com>
2022-09-14 19:53:13 +0000

    Add C function that computes the absolute value of an integer

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,22 +7,17 @@
 
 int print_last_digit(int n)
 {
-	n = n % 10;
+	int l;
 
-	if (n < 0)
+	l = n % 10;
+	if (l < 0)
 	{
-		int l;
-		l = n % 10;
-		if (l < 0)
-		{
-			_putchar(-l + 48);
-			return (-l);
-		}
-		
-		else
-		{
-			_putchar(l + 48);
-			return (l);
-		}
+		_putchar(-l + 48);
+		return (-l);
+	}
+	else
+	{
+		_putchar(l + 48);
+		return (l);
 	}
 }
