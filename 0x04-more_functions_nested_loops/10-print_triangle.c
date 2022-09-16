bf0eaaa16c40c1b0ecbc08dc4df30227171ce813
Lydia Taban <tabanlydia@gmail.com>
2022-09-16 19:56:33 +0000

    Add C function that prints a triangle followed by a new line

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,26 +9,23 @@ void print_triangle(int size)
 {
 	int a, b, c;
 
-	a = 0;
-	b = size - 1;
-	while (a < size)
-	{
-		b = size - 1 - a;
-		c = a + 1;
-		while (b > 0)
-		{
-			_putchar(' ');
-			b--;
-		}
-		while (c > 0)
-		{
-			_putchar('#');
-			c--;
-		}
-		_putchar('\n');
-		c++;
-	}
-
 	if (size <= 0)
+	{
 		_putchar('\n');
+	}
+	else
+	{
+		for (b = 0; b <= size - 1; b++)
+		{
+			for (a = 0; a <= size - 1 - b; a++)
+			{
+				_putchar('_');
+			}
+			for (c = 0; c <= b; c++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }
