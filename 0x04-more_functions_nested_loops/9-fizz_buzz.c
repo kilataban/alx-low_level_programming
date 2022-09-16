ff5961c56ef6f6efff9ba1fe90936341b8fb34f9
Lydia Taban <tabanlydia@gmail.com>
2022-09-16 19:17:19 +0000

    Add C fizzbuzz solution

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+* main - invoke fizbuzz for numbers 1 to 100
+* Description: multiples of three - Fizz, multiples of five - Buzz
+* multiples of both 3 & 5 - FizzBuzz.
+* Return (0)
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 1;
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+
+		if (i != 100)
+			printf(" ");
+		i++;
+	}
+	printf("\n");
+
+	return (0);
+}
