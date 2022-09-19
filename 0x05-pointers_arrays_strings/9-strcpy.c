60e001fe83497fb087c70cbc980eb7de6ff287ce
Lydia Taban <tabanlydia@gmail.com>
2022-09-19 19:36:30 +0000

    Add C program that copies the string pointed to by src, including the terminating null byte (/0), to the buffer pointed to by dest

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _strcpy - copies the string pointed to by src, to buffer pointed by dest 
+ * _strcpy - copies the string pointed to by src, to buffer pointed by * dest
  * @dest: destination
  * @src: source
  * Return: the pointer to dest
