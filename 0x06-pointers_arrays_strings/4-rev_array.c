3a55d36e6985554a14429627e590a809407f56a0
Lydia Taban <tabanlydia@gmail.com>
2022-09-22 17:41:50 +0000

    Add C program that reverses the content of an array of integers.

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* reverse_array - reverses the content of an array of integers
+* @a: array of integers
+* @n: number of elements in the array
+*/
+
+void reverse_array(int *a, int n)
+{
+	int x;
+	int y;
+
+	for (x = 0; x < (n / 2); x++)
+	{
+		y = a[x];
+		a[x] = a[n - x - 1];
+		a[n - x - 1] = y;
+	}
+}
