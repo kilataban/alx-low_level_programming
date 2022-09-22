fdf6aefacea516e5b4ba76bc4d1732a49169cf77
Lydia Taban <tabanlydia@gmail.com>
2022-09-22 17:28:02 +0000

    Add C program that compares two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _strcmp - compares two strings
+* @s1: first string to compare
+* @s2: second string to compare
+* Return: 0 if equal
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, x = 0;
+
+	while (x == 0)
+	{
+		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+			break;
+		x = *(s1 + i) - *(s2 + i);
+		i++;
+	}
+
+	return (x);
+}
+
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,6 +4,6 @@
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *scr, int n);
 char *_strncopy(char *dest, char *src, int n);
-
+int _strcmp(char *s1, char *s2);
 
 #endif /* _MAIN_H_ */
