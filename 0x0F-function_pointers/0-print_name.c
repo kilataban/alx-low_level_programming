16bd65aa355a51a0f0bcf5f98814784eecf9171d
Lydia Taban <tabanlydia@gmail.com>
2022-10-13 18:45:28 +0000

    Write a function that prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints name
+ * @name: name to print
+ * @f: pointer to the print_name function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
