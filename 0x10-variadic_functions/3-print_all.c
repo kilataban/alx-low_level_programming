296d5a323e8ff77cfe4c65ceb5cb331a4ba38166
Lydia Taban <tabanlydia@gmail.com>
2022-10-14 22:05:03 +0000

    Add C program that yields a function which prints all arguments passed to it

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,49 +3,90 @@
 #include "variadic_functions.h"
 
 /**
- * print_all - prints any arguments passed to it
- * @format: the data type to match to args
+ * print_c - print a char
+ * @c: char to print
  *
- * Return: Always nothing for void function type
+ * Return: void
  */
-
-void print_all(const char *const format, ...)
+void print_c(va_list c)
 {
-	unsigned int i = 0;
-	char *arg_s;
-	va_list args;
+	printf("%c", va_arg(c, int));
+}
 
-	va_start(args, format);
-	while (*(format + i) != '\0' && (format))
+/**
+ * print_s - prints a string
+ * @s: string to print
+ *
+ * Return: void
+ */
+void print_s(va_list s)
+{
+	char *str = va_arg(s, char *);
+
+	if (str == NULL)
+		str = "(nil)";
+	printf("%s", str);
+}
+
+/**
+ * print_i - prints an int
+ * @i: int to print
+ *
+ * Return: void
+ */
+void print_i(va_list i)
+{
+	printf("%d", va_arg(i, int));
+}
+
+/**
+ * print_f - prints a float
+ * @f: float to print
+ *
+ * Return: void
+ */
+void print_f(va_list f)
+{
+	printf("%f", va_arg(f, double));
+}
+
+/**
+ * print_all - prints anything
+ * @format: list of argument types passed to the function
+ *
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	unsigned int i, j;
+	print_t p[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"i", print_i},
+		{"f", print_f},
+		{NULL, NULL}
+	};
+	va_list valist;
+	char *separator = "";
+
+	va_start(valist, format);
+	i = 0;
+	while (format && format[i])
 	{
-		switch (*(format + i))
+		j = 0;
+		while (p[j].t != NULL)
 		{
-		case 's':
-			arg_s = va_arg(args, char *);
-			if (arg_s == NULL)
+			if (*(p[j].t) == format[i])
 			{
-				printf("(nil)");
+				printf("%s", separator);
+				p[j].f(valist);
+				separator = ", ";
 				break;
 			}
-			printf("%s", arg_s);
-			break;
-		case 'i':
-			printf("%d", va_arg(args, int));
-			break;
-		case 'f':
-			printf("%f", (float) va_arg(args, double));
-			break;
-		case 'c':
-			printf("%c", (char) va_arg(args, int));
-			break;
-		default:
-			i++;
-			continue;
+			j++;
 		}
-		if (*(format + i + 1) != '\0')
-			printf(", ");
 		i++;
 	}
+	va_end(valist);
 	printf("\n");
-	va_end(args);
 }
