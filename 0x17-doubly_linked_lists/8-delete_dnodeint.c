89bd5c36d7d9b3c2ebe2261b187e25224c6d48eb
Lydia Taban <tabanlydia@gmail.com>
2022-12-08 20:40:33 +0000

    Delete the nodes at specified index within a linked list

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - Delete node at index of a doubly linked list
+ * @head: list header double pointer
+ * @index: index position to remode linked list
+ * Return: 1 for Success, -1 failed
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *tmp, *next, *prev;
+	unsigned int c;
+
+	if (*head == NULL)
+		return (-1);
+
+	for (tmp = *head, c = 0, prev = NULL; tmp && c < index; c++)
+	{
+		prev = tmp;
+		tmp = tmp->next;
+	}
+
+	if (tmp == NULL)
+		return (-1);
+
+	next = tmp->next;
+	if (prev == NULL)
+	{
+		free(tmp);
+		if (next != NULL)
+		{
+			next->prev = NULL;
+			*head = next;
+		}
+		else
+			*head = NULL;
+	}
+	else if (next == NULL)
+	{
+		prev->next = NULL;
+		free(tmp);
+	}
+	else
+	{
+		prev->next = next;
+		next->prev = prev;
+		free(tmp);
+	}
+
+	return (1);
+}
